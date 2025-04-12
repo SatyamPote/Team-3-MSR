a7c991734fbcf4ece2224c8d428488a4b4258b64
DhivyaBalakumar <161305232+DhivyaBalakumar@users.noreply.github.com>
2025-04-12 13:20:14 +0530

    Update 15 main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import time
-from datetime import datetime
 from extract import extract_imdb_data
+from datetime import datetime
 
 def run_every_minute():
-    print("Starting fetch every 1 minute...\nPress Ctrl+C to stop.\n")
-    try:
-        while True:
-            print("="*50)
-            print(f"Running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-            extract_imdb_data()
-            print("Waiting for 60 seconds...\n")
-            time.sleep(60)
-    except KeyboardInterrupt:
-        print("\nStopped by user.")
+    while True:
+        print("\n" + "=" * 50)
+        print(f"🚀 Running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        extract_imdb_data()
+        time.sleep(60)
 
 if __name__ == "__main__":
+    print("🟢 IMDB movie fetch started. Auto-saving CSV every 1 minute...\nPress Ctrl+C to stop.")
     run_every_minute()
