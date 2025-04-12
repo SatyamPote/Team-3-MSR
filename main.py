09197056db21b3e1433324036f9fdbff092d5a67
DhivyaBalakumar <161305232+DhivyaBalakumar@users.noreply.github.com>
2025-04-12 19:56:19 +0530

    Update 16 main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import time
-from extract import extract_imdb_data
-from datetime import datetime
-
-def run_every_minute():
-    while True:
-        print("\n" + "=" * 50)
-        print(f"🚀 Running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        extract_imdb_data()
-        time.sleep(60)
+# main.py
+from extract import extract_data
 
 if __name__ == "__main__":
-    print("🟢 IMDB movie fetch started. Auto-saving CSV every 1 minute...\nPress Ctrl+C to stop.")
-    run_every_minute()
+    video_url = input("Enter the YouTube video URL: ")
+    extract_data(video_url)
