8ce45ccf2b95eb4e149f4ae7bba2b411cda52e7f
DhivyaBalakumar <161305232+DhivyaBalakumar@users.noreply.github.com>
2025-04-15 09:29:13 +0530

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,10 +1,10 @@
 # transform.py
-def transform_movies_data(data):
-    transformed = []
-    for movie in data:
-        transformed.append({
-            "Title": movie['title'],
-            "Year": int(movie['year']),
-            "Rating": float(movie['rating']) if movie['rating'] != 'N/A' else None
-        })
+def transform_metric_data(raw_data):
+    # This can be used for scaling, cleaning, or formatting
+    transformed = {
+        "timestamp": raw_data["timestamp"],
+        "video_id": raw_data["video_id"],
+        "metric": raw_data["metric"],
+        "value": int(raw_data["value"]) if raw_data["value"] is not None else 0
+    }
     return transformed
